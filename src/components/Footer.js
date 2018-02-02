785849792ab972bd2b58e82ab4b8256f4919c4c3
Chris Jannenga <cjannenga312@gmail.com>
2018-02-01 23:05:18 -0700

    footer text in vertical rows

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,33 @@ import '../styles/Footer.css'
 class Footer extends React.Component {
   render () {
     return (
-      <div></div>
+      <div className='blue-top-bg'>
+        <div className='row'>
+          <div className='col s6 m6 l6 offset-s3 offset-m3 offset-l3 center'>
+            <h5 className='white-text'>Clients: Start Your Journey</h5>
+          </div>
+          <div className='col s6 m6 l6 offset-s3 offset-m3 offset-l3 center'>
+            <h5 className='white-text'>Proffesionals: Work with us</h5>
+          </div>
+          <div className='col s6 m6 l6 offset-s3 offset-m3 offset-l3 center'>
+            <h5 className='white-text'>Peep Our Process</h5>
+          </div>
+          <div className='col s6 m6 l6 offset-s3 offset-m3 offset-l3 center'>
+            <h5 className='white-text'>Visit Our Ventures</h5>
+          </div>
+          <div className='col s6 m6 l6 offset-s3 offset-m3 offset-l3 center'>
+            <h5 className='white-text'>Jump into our Journal</h5>
+          </div>
+          <div className='col s6 m6 l6 offset-s3 offset-m3 offset-l3 center'>
+            <h5 className='white-text'>Codename: InstAwesome</h5>
+          </div>
+          <div className='col s6 m6 l6 offset-s3 offset-m3 offset-l3 center'>
+            <h5 className='white-text'>IdeaPortals.com</h5>
+          </div>
+        
+        </div>
+
+      </div>
     )
   }
 }
