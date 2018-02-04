42266240a15983e834592d63359ba5256c408dbb
Johnny Capra <johnnycapra@Johnnys-MacBook-Pro.local>
2018-02-04 10:49:03 -0700

    mobile nav works perfect

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,8 +6,8 @@ class NavBar extends React.Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a href="#" className="brand-logo left">Logo</a>
-          <ul id="nav-mobile" className="right">
+          <a href="#" id="nav-mobile" className="brand-logo left button-collapse">Logo</a>
+          <ul id="nav-mobile" className="right button-collapse">
             <li><a href="sass.html">Process</a></li>
             <li><a href="badges.html">Ventures</a></li>
             <li><a href="collapsible.html">Community</a></li>
