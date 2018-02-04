20779671923619de9a7225bef6fcb39d828b469a
Chris Jannenga <cjannenga312@gmail.com>
2018-02-04 10:32:21 -0700

    navbar work

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,11 +9,13 @@ class NavBar extends React.Component {
   render () {
 
     return (
-      <div>
+      <div className='bottom-padding'>
         <div className='row no-padding no-margin blue-top-bg'>
-          <div className='col s10 m4 l4 offset-s1'>
-            <img src='https://i.imgur.com/HJHf2gc.png' alt='logo' height='100px'/>
-            
+          <div className='col s10 m3 l3 offset-s1'>
+            <img src='https://i.imgur.com/HJHf2gc.png' alt='logo' height='100vh'/>
+          </div>
+          <div className='col s10 m3 l3 offset-s1  center'>
+            <h6 className='white-text asap-font'>Process</h6>
           </div>
         </div>
       </div>
