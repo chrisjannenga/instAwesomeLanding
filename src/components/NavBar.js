93691703f2dd1d939ee2c9627bdaf1e724349220
Chris Jannenga <cjannenga312@gmail.com>
2018-02-04 11:51:10 -0700

    navbar testing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,12 +9,12 @@ class NavBar extends React.Component {
   render () {
 
     return (
-      <div className='bottom-padding'>
+      <div className=''>
         <div className='row no-padding no-margin blue-top-bg'>
           <div className='col s10 m3 l3 offset-s1'>
             <img src='https://i.imgur.com/HJHf2gc.png' alt='logo' height='100vh'/>
           </div>
-          <div className='col s10 m3 l3 offset-s1  center'>
+          <div className='col s10 m3 l3 offset-s1 right margin-top  center'>
             <h6 className='white-text asap-font'>Process</h6>
           </div>
         </div>
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,14 +1,52 @@
 import React, { Component } from 'react';
 import '../styles/Testimonials.css'
 
+
 class Testimonials extends Component {
 
+
+  state = {
+    counter: 0,
+    currentT: []
+  }
+
+  text = ["Welcome", "Hi", "Sup dude",
+  `this is a really long testimony about cool stuff. Work with us and we will make
+   you the most successful version of yourself there could be.
+   Taco bell is kinda sweet sometimes. One time I ate a jalapeno.`
+  ];
+
+  currentT = [];
+  counter = 0;
+
+  change = () => {
+    this.currentT = this.text[this.counter];
+    console.log(this.currentT);
+    this.setState({
+      counter: this.counter,
+      currentT: this.currentT
+    })
+    this.counter++;
+    if (this.counter >= this.text.length) {
+      this.counter = 0;
+    }
+  }
+
+
   render () {
+
+    console.log(this.state);
+
+    setTimeout(this.change, 5000)
+
     return (
-      <div className=''>
+      <div className='testimony'>
+        <div className='currentT'>
+          <p>{this.state.currentT}</p>
+        </div>
       </div>
     )
   }
 }
 
-export default Testimonials
+export default Testimonials;
