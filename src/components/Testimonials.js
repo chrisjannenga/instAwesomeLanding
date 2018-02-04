291a95756cee1c6aeb0ee02cb535b57baa3b2fea
Johnny Capra <johnnycapra@Johnnys-MacBook-Pro.local>
2018-02-04 09:23:31 -0700

    fixed some styles

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -8,11 +8,12 @@ class Testimonials extends Component {
   var text = ["Welcome", "Hi", "Sup dude"];
   var counter = 0;
   var currentT= document.getElementById("changeText");
-  setInterval(change, 1000);
+  setInterval(change, 1000)
 
   function change() {
    currentT = text[counter];
    counter++;
+   console.log(currentT, "Testimonials");
    if (counter >= text.length) {
      counter = 0;
    }
@@ -21,7 +22,7 @@ class Testimonials extends Component {
     return (
       <div className='testimony'>
         <div className='currentT'>
-          <p onLoad={ setInterval(change, 1000)} ref="changeText" >
+          <p ref="changeText" >
           </p>
         </div>
       </div>
